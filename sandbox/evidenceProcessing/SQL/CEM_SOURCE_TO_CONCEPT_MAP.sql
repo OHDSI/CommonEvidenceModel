92c26e6c5d11ad4629f450c56427e5daa39d4289
Lee Evans <root@ip-172-31-44-228.ec2.internal>
2017-07-09 17:36:12 +0000

    begin separation of stcm mappings & shared EU_ADR SQL

--- sandbox/evidenceProcessing/SQL/CEM_SOURCE_TO_CONCEPT_MAP.sql
+++ sandbox/evidenceProcessing/SQL/CEM_SOURCE_TO_CONCEPT_MAP.sql
@@ -1,17 +1,3 @@
-IF OBJECT_ID('@fqTableName','U') IS NOT NULL DROP TABLE @fqTableName;
-
-CREATE TABLE @fqTableName (
-	source_code             varchar(255) NOT NULL,
-	source_concept_id       int NOT NULL,
-	source_vocabulary_id    varchar(20) NOT NULL,
-	source_code_description varchar(255) NULL,
-	target_concept_id       int NOT NULL,
-	target_vocabulary_id    varchar(20) NULL,
-	valid_start_date        date NOT NULL,
-	valid_end_date          date NOT NULL,
-	invalid_reason          varchar(1) NULL
-);
-
 /*Genderate Temporary UMLS Lookup*/
 SELECT DISTINCT CUI, SAB AS SOURCE_CODE_TYPE, MIN(c.concept_id) AS CONCEPT_ID
 INTO #TEMP_1
